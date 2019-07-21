39962caf13ac95405859a07f12f8e2178c93e9d8
dongzhb <dongzhb@fxiaoke.com>
2019-07-21 10:21:31 +0800

    代码结构

---
+++ wywhdgg-conf-admin/src/main/java/com/wywhdgg/dzb/WywhdggConfAdminApplication.java
@@ -0,0 +1,11 @@
+package com.wywhdgg.dzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WywhdggConfAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WywhdggConfAdminApplication.class, args);
+    }
+}
---
+++ wywhdgg-conf-spingboot/src/main/java/com/wywhdgg/dzb/WywhdggConfSpingbootApplication.java
@@ -0,0 +1,11 @@
+package com.wywhdgg.dzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WywhdggConfSpingbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WywhdggConfSpingbootApplication.class, args);
+    }
+}
